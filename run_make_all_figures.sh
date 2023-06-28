e1dab85a268b9206a2a4c1091d1cecdf3dfa318b
Aaron Kujawa <askujawa@gmail.com>
2023-06-28 15:37:44 +0100

    Deleted files

--- run_make_all_figures.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python scripts/make_ga_partition_figure.py
-python scripts/make_ga_results_figure.py
-python scripts/make_results_figure.py
-python scripts/make_scoring_results.py
