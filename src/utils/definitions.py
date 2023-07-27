d75040ab5bc37d712ee61088de9a69f3dc718793
Aaron Kujawa <askujawa@gmail.com>
2023-07-27 14:25:57 +0100

    Updated max number of iterations for nonlinear registration to match GIF

--- src/utils/definitions.py
+++ src/utils/definitions.py
@@ -6,7 +6,7 @@ reg_aladin_LP = 2  # Number of levels to use to run the registration once the py
 # NONLINEAR REGISTRATION HYPER-PARAMETERS
 reg_f3d_JL = 0.0001  # Weight of log of the Jacobian determinant penalty term
 reg_f3d_BE = 0.005  # Weight of the bending energy (second derivative of the transformation) penalty term
-reg_f3d_MAXIT = 100  # Maximal number of iteration at the final level
+reg_f3d_MAXIT = 250  # Maximal number of iteration at the final level
 reg_f3d_LN = 4  # Number of level to perform
 reg_f3d_LP = 3  # default 3; we do only the lp first level of the pyramid
 reg_f3d_GRID_SPACING = -5.0  # Final grid spacing along the x, y, z axes in mm (in voxel if negative value), corresponds to -sx option in reg_f3d
